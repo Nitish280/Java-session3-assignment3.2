9f74eb09d87dd5dfcd23909b5d357d11f2f47f72
Nitish280 <nsrccs@gmail.com>
2017-06-07 19:38:21 +0530

    Update TruthTable.java

--- src/TruthTable.java
+++ src/TruthTable.java
@@ -5,15 +5,17 @@ public class TruthTable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in  = new Scanner(System.in);
-		
+		//we take the input from the user in byte form
 		System.out.println("Input first number: ");  
 	    byte num1 = in.nextByte();
-	    		
-	    System.out.println("Input first number: ");
+	    	//we take another input in byte form	
+	    System.out.println("Input Second number: ");
 	    byte  num2= in.nextByte();
 	    
 	    int a=num1,b=num2;
+		//we initialise here bitwise for this condition
 	    System.out.println(a&b); 
+		//we initialise here logical 
 	    System.out.println(a==1 && b==1);
 	 
 	    in.close();
